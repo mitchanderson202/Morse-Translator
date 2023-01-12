ee69ab29dae8c13904d58027bb50085a501fc353
Mitchell Anderson <mitchellanderson202@hotmail.com>
2023-01-12 20:13:16 +1100

    updated form functionality

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-let englishTextarea = document.querySelector(".main__english--text");
 let englishToMorse = document.querySelector(".main__english--translator");
-let morseTextarea = document.querySelector(".main__morse--text");
-let morseToEnglish = document.querySelector(".main__morse--translator");
+let englishSubmit = document.querySelector(".main__english--submit");
 
-englishTextarea.addEventListener("input", (e) => {
-  console.log(e.target.value);
-  englishToMorse.append(e.target.value);
+let morseToEnglish = document.querySelector(".main__morse--translator");
+let morseSubmit = document.querySelector(".main__morse--submit");
+
+// englishSubmit.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log(englishTextareaValue);
+// });
+
+englishSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let englishTextareaValue = document.querySelector(
+    ".main__english--text"
+  ).value;
+
+  englishToMorse.textContent = englishTextareaValue;
+
+  document.querySelector(".main__english--text").value = "";
 });
 
-morseTextarea.addEventListener("input", (e) => {
-  console.log(e.target.value);
-  morseToEnglish.append(e.target.value);
+morseSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let morseTextaresValue = document.querySelector(".main__morse--text").value;
+
+  morseToEnglish.textContent = morseTextaresValue;
+
+  document.querySelector(".main__morse--text").value = "";
 });
